a89e33aac5e23f6f834809a6a35b539a059169f5
Kanon <540004716@qq.com>
2017-10-01 00:24:13 +0800

    Create Exe12.java

---
+++ newcoder/Exe12.java
@@ -0,0 +1,26 @@
+/*题目描述：给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
+
+  笔记：分情况讨论  */
+  
+  
+  public class Solution {
+    public double Power(double base, int exponent) {
+        double tmp=base;
+        if(exponent==0){
+            return 1;
+        }
+        else if(exponent>0){
+            for(int count=1;count<exponent;count++){
+                base=base*tmp;
+            }
+            return base;
+    	}
+    	else if(exponent<0){
+            for(int count=1;count<(-exponent);count++){
+                base=base*tmp;
+            }
+            return 1/base;
+        }
+        return -1;
+  }
+}
