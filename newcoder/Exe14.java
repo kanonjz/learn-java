f36e02d11c292f0f0c675cc938f104736b01448d
Kanon <540004716@qq.com>
2017-10-01 17:10:42 +0800

    Create Exe14.java

---
+++ newcoder/Exe14.java
@@ -0,0 +1,43 @@
+/*题目描述：输入一个链表，输出该链表中倒数第k个结点。
+
+  笔记：用堆栈来存储ListNode  */
+  
+  
+  
+  /*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+import java.util.Stack;
+
+public class Solution {
+	public ListNode FindKthToTail(ListNode head,int k) {
+        
+       	if(head==null){
+            return null;
+        }
+        
+		Stack<ListNode> s = new Stack<ListNode>();
+        
+        s.push(head);      
+        int count=1;
+	    while(head.next!=null){
+	    	head=head.next;
+	    	s.push(head);
+            count++;
+	    }
+	      	
+	    if(k==0||k>count){
+           return null;
+        } 
+	    for(int i=1;i<=(k-1);i++){
+	        s.pop();
+	    }
+	    return s.pop();
+	}
+}
