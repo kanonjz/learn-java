098322da6f3a2a8010657365d9374abc0829da6c
Kanon <540004716@qq.com>
2017-10-07 23:58:12 +0800

    Update Exe19.java

--- newcoder/Exe19.java
+++ newcoder/Exe19.java
@@ -11,16 +11,16 @@ public class TreeNode {
 
 }
 */
-public class Solution {
-    public void Mirror(TreeNode root) {
-        if(root==null){
-            return;
-        }
-        TreeNode temp;
-        temp=root.left;
-        root.left=root.right;
-        root.right=temp;
-        Mirror(root.left);
-        Mirror(root.right);
-    }
-}
+链接：https://www.nowcoder.com/questionTerminal/564f4c26aa584921bc75623e48ca3011
+来源：牛客网
+
+class Solution {
+public:
+    void Mirror(TreeNode *p) {
+    if(p){
+            swap(p -> left, p -> right);
+            Mirror(p -> left);
+            Mirror(p -> right);
+        }
+    }
+};
